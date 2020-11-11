9f95970faa4958a198dab2b21aac30e80ce20be9
John Rowan <JR897843@wcupa.edu>
2020-11-11 18:37:36 -0500

    got cat and rabbit model and put them in my scene to mess with

---
+++ Assets/John/mouseRun.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseRun : MonoBehaviour
+{
+    private Animator m_animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_animator = GetComponent<Animator>();
+        m_animator.SetInteger("AnimIndex", 1);
+        m_animator.SetTrigger("Next");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
