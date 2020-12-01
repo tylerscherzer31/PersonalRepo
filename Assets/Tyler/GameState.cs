5dd4128614ae433afa65df1734e18005d525b709
tylerscherzer31 <30841349+tylerscherzer31@users.noreply.github.com>
2020-12-01 12:06:13 -0500

    GameState script made and attached to main camera

---
+++ Assets/Tyler/GameState.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+    public enum GameMode
+    {
+        idle,
+        playing,
+        dead,
+        levelEnd,
+        end
+    }
+
+    public static int numLives = 3;
+    public static GameMode mode = GameMode.idle;
+
+}
+
