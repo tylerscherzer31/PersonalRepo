2e881f40e369b0e4822b5630bca901c993431e9e
John Rowan <JR897843@wcupa.edu>
2020-12-01 14:49:18 -0500

    added mouse movement

---
+++ Assets/John/MouseControls.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseControls : MonoBehaviour
+{
+    public GameObject[] positions;
+    public bool reset = true;
+    public int positionIndex = 1;
+    void Start()
+    {
+        transform.position = positions[positionIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(Input.GetAxis("Horizontal"));
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            
+                if (positionIndex - 1 > -1)
+                {
+                    positionIndex -= 1;
+                    transform.position = positions[positionIndex].transform.position;
+                    reset = false;
+
+                }
+
+            
+
+
+        }
+        else if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            
+                if (positionIndex + 1 < positions.Length)
+                {
+                    positionIndex += 1;
+                    transform.position = positions[positionIndex].transform.position;
+                    reset = false;
+
+                }
+            
+            
+                
+            
+        }
+        else
+        {
+            reset = true;
+        }
+    }
+}
