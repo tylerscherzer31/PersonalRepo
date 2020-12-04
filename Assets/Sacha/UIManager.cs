1f4e3561eae97db83074d2100a1bf3d684a102dd
sxchxmxrie <70333050+sxchxmxrie@users.noreply.github.com>
2020-12-04 12:42:47 -0500

    scripting the UI

---
+++ Assets/Sacha/UIManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject pauseScreen;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            pauseScreen.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+
+    public void Continue()
+    {
+        pauseScreen.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void Quit()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
