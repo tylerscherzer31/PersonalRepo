abb577a2c0d5b04c00814b6f308a8842a4882897
tylerscherzer31 <30841349+tylerscherzer31@users.noreply.github.com>
2020-12-04 10:45:09 -0500

    Main and End menu done with funtioning buttons

--- Assets/Tyler/MainMenu.cs
+++ Assets/Tyler/MainMenu.cs
@@ -14,4 +14,9 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
 }
