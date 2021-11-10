ac0e33721029f2cde79dcbca39884fd67e113414
Tyler Scherzer <30841349+tylerscherzer31@users.noreply.github.com>
2021-11-10 17:15:19 -0500

    Create App.js

---
+++ NodeJSFinalProject/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import AcademicDistrubitiveAndAdditional from "./AcademicDistrubitiveAndAdditional";
+
+export default class App extends Component {
+    
+    render() {
+        return <AcademicDistrubitiveAndAdditional/>
+    }
+}
